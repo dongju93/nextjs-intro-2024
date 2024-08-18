fdd90424d4ee11bfcbce29262618695c8f2c3afc
dongju93 <spdlqj011@gmail.com>
2024-08-19 00:42:03 +0900

    #2.8 Metadata

--- app/about-us/layout.tsx
+++ app/about-us/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "About Us",
+    description: "Dummy page",
+};
+
 export default function AboutUsLayout({
     children,
 }: {
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,13 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Not Found",
+};
+
 export default function NotFound() {
-  return (
-    <div>
-      <h1>404 - Not Found</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>404 - Not Found</h1>
+        </div>
+    );
 }
