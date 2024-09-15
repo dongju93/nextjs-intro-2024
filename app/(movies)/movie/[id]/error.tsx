c24c3d252bd4ba34ccdc092f0d7972ff9a4fdb66
dongju93 <spdlqj011@gmail.com>
2024-09-15 14:08:50 +0900

    #3.7 Error Handling

---
+++ app/(movies)/movie/[id]/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ErrorMoviesPage() {
+    return <h1>Something went wrong</h1>;
+}
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,9 +1,10 @@
 import { api_url } from "../components/urls";
 
 const getVideo = async (id: string) => {
-    const response = await fetch(`${api_url}/${id}/videos`);
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    return response.json();
+    // await new Promise((resolve) => setTimeout(resolve, 5000));
+    throw new Error("Error in fetching videos");
+    // const response = await fetch(`${api_url}/${id}/videos`);
+    // return response.json();
 };
 
 export default async function MovieVideos({ id }: { id: string }) {
